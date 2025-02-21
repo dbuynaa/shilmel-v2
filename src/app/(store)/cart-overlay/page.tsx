c02f1aedecb9b1d3d1fc6104a52626f50a9b9f22
Dbuynaa <dbuynaa01@gmail.com>
2025-02-21 16:14:18 +0800

    sda

--- src/app/(store)/cart-overlay/page.tsx
+++ src/app/(store)/cart-overlay/page.tsx
@@ -1,18 +1,7 @@
-"use client"
-
-import { useEffect } from "react"
-import { useRouter } from "next/navigation"
-
-export const dynamic = "force-static"
+import { redirect } from "next/navigation"
 
 // This page is rendered on full reload
 // We want to redirect to `/cart` to avoid conflict of the routes
 export default function RedirectToCart() {
-  const router = useRouter()
-
-  useEffect(() => {
-    router.replace("/cart")
-  }, [router])
-
-  return null
+  redirect("/cart")
 }
